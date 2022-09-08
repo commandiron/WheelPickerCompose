b6bbf96fb8ce94c9e5d8eec1d92325add7fc717e
Emir Demirli <commandiron@gmail.com>
2022-09-08 16:52:35 +0300

    commit.

--- app/src/main/java/com/commandiron/wheelpickercompose/MainActivity.kt
+++ app/src/main/java/com/commandiron/wheelpickercompose/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.commandiron.wheel_picker_compose.*
 import com.commandiron.wheelpickercompose.ui.theme.WheelPickerComposeTheme
@@ -34,8 +38,20 @@ class MainActivity : ComponentActivity() {
                         WheelTimePicker { snappedTime -> }
                         WheelTextPicker(texts = (1..6).map { it.toString() })
                         WheelPicker(count = 6) { index, snappedIndex ->
-                            Card(Modifier.size(128.dp).padding(8.dp)) {}
+                            Card(Modifier
+                                .size(128.dp)
+                                .padding(8.dp)) {}
                         }
+//                        WheelDateTimePicker(
+//                            size = DpSize(200.dp, 100.dp),
+//                            textStyle = MaterialTheme.typography.titleSmall,
+//                            textColor = Color(0xFFffc300),
+//                            infiniteLoopEnabled = true,
+//                            selectorEnabled = true,
+//                            selectorShape = RoundedCornerShape(0.dp),
+//                            selectorColor = Color(0xFFf1faee).copy(alpha = 0.2f),
+//                            selectorBorder = BorderStroke(2.dp, Color(0xFFf1faee))
+//                        ) { snappedDate, snappedTime -> }
                     }
                 }
             }
